52de3594597e3313e7e1b4a7cd30d5e65f2da574
Patrick Burke <W0480974@nscc.ca>
2024-02-15 17:38:44 -0400

    Modified bet function unit tests

--- test/Bet.test.js
+++ test/Bet.test.js
@@ -2,35 +2,42 @@ import { expect, test } from 'vitest'
 import { shallowMount } from '@vue/test-utils'
 import Blackjack from '../src/components/Blackjack.vue';
 
-test('Test betUp function', () => {
+test('Test that betUp function increases bet by 5', () => {
+    //create instance of Blackjack component in jsdom for testing, set initial values
     const wrapper = shallowMount(Blackjack);
-    expect(wrapper.vm.bet).toBe(0);
+    wrapper.vm.bet = 0;
 
     //Test that bet is equal to 5 after calling betUp function
     wrapper.vm.betUp();
     expect(wrapper.vm.bet).toBe(5);
-
-    //Test that betUp will not increase above players money
-    const money = wrapper.vm.money;
-    for (let i=0; i < 100; i++) {
-        wrapper.vm.betUp();
-    }
-
-    expect(wrapper.vm.bet).toBe(money);
 });
 
-test('Test betDown function', () => {
+test('Test that betUp function will not increase bet above money', () => {
+    //create instance of Blackjack component in jsdom for testing, set initial values
+    const wrapper = shallowMount(Blackjack);
+    wrapper.vm.bet = 100;
+    wrapper.vm.money = 100;
+
+    //Test that bet is still equal to 100 after calling betUp
+    wrapper.vm.betUp();
+    expect(wrapper.vm.bet).toBe(100);
+});
+
+test('Test that betDown function decreases bet by 5', () => {
+    //create instance of Blackjack component in jsdom for testing, set initial values
     const wrapper = shallowMount(Blackjack);
     wrapper.vm.bet = 10;
 
     //Test that betDown function decreases bet by 5
     wrapper.vm.betDown();
     expect(wrapper.vm.bet).toBe(5);
+});
 
-    //Test that betDown will not decrease bet below 0
-    for (let i=0; i < 100; i++) {
-        wrapper.vm.betDown();
-    }
+test('Test that betDown function will not decrease bet below 0', () => {
+    const wrapper = shallowMount(Blackjack);
+    wrapper.vm.bet = 0;
 
+    //Test that bet is still equal to 0
+    wrapper.vm.betDown();
     expect(wrapper.vm.bet).toBe(0);
 });
